ad5fce6b0dc954baa9a09001aaefae125751c072
hitbit72 <hitbit2@hotmail.com>
2026-08-10 17:58:59 +0200

    templates2

--- wisp_portal/settings.py
+++ wisp_portal/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'wisp_portal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'templates2'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
