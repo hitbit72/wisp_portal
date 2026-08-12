bde8d0588c086cbd13c50eae9cf4eebd7af458ab
hitbit72 <hitbit2@hotmail.com>
2026-08-12 10:17:46 +0200

    .

--- mikrotik/views.py
+++ mikrotik/views.py
@@ -7,7 +7,6 @@ from red.models import Sector
 
 from .forms import PlanForm, RouterForm
 from .models import Plan, Router
-from .urls import mikrotik
 
 @login_required
 def lista_routers(request):
@@ -49,7 +48,7 @@ def lista_routers(request):
 def detalle_router(request, pk):
     router = get_object_or_404(Router.objects.prefetch_related('planes'), pk=pk)
     # Obtiene la URL anterior, o asigna una ruta por defecto si no existe
-    url_anterior = request.META.get('HTTP_REFERER', mikrotik:lista)
+    url_anterior = request.META.get('HTTP_REFERER', '/mikrotik/')
     return render(request, 'mikrotik/detalle.html', {'router': router, 'url_anterior': url_anterior})
 
 
