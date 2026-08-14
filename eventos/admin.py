c38b925f91cdbe354f530f511c6150beb639c245
hitbit72 <hitbit2@hotmail.com>
2026-08-14 17:56:17 +0200

    .

--- eventos/admin.py
+++ eventos/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-
 from .models import Evento
 
 
 @admin.register(Evento)
 class EventoAdmin(admin.ModelAdmin):
-    """Solo lectura: los eventos se generan desde los módulos, nunca a mano."""
+    """Solo lectura: los eventos se generan desde los módulos, nunca a mano.
+    Modificación: desde el administración DJango si se pueden modificar"""
     list_display = ('fecha', 'nivel', 'titulo', 'modulo', 'leido', 'user')
     list_filter = ('nivel', 'leido', 'modulo')
     search_fields = ('titulo', 'texto', 'modulo')
-    readonly_fields = ('fecha', 'nivel', 'titulo', 'texto', 'modulo', 'leido', 'user')
+    #readonly_fields = ('fecha', 'nivel', 'titulo', 'texto', 'modulo', 'leido', 'user')
