9a2724d27261ce98bfdb485d1151077f9ff7aa88
hitbit72 <hitbit2@hotmail.com>
2026-08-19 08:33:20 +0200

    model Dispositivo, alarma de puerto desconectado

--- red/models.py
+++ red/models.py
@@ -111,6 +111,7 @@ class Dispositivo(models.Model):
 
     escanear = models.BooleanField(default=False, null=True, blank=True)
     alarma = models.BooleanField(default=False, null=True, blank=True)
+    alarma_puerto = models.BooleanField(default=False, null=True, blank=True, verbose_name='Alarma puertos')
     oid = models.ForeignKey(Marca, null=True, blank=True, on_delete=models.SET_NULL, related_name='dispositivos')
     
     latitud = models.DecimalField(max_digits=10, decimal_places=7, null=True, blank=True)
