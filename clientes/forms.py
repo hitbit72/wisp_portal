0b2811e0496bf0d7476bb5bfe06eb01925fcdbc7
hitbit72 <hitbit2@hotmail.com>
2026-08-20 20:05:56 +0200

    .

--- clientes/forms.py
+++ clientes/forms.py
@@ -3,25 +3,6 @@ from .models import Cliente, Contrato
 
 from core.utils import BootstrapFormMixin
 
-"""
-class BootstrapFormMixin:
-    """Agrega automáticamente las clases de Bootstrap a cada campo, para no
-    tener que repetirlas a mano en cada formulario."""
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for campo in self.fields.values():
-            widget = campo.widget
-            if isinstance(widget, forms.CheckboxInput):
-                css_extra = 'form-check-input'
-            elif isinstance(widget, (forms.Select, forms.SelectMultiple)):
-                css_extra = 'form-select'
-            else:
-                css_extra = 'form-control'
-            actual = widget.attrs.get('class', '')
-            widget.attrs['class'] = f'{actual} {css_extra}'.strip()
-"""
-
 class ClienteForm(BootstrapFormMixin, forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
